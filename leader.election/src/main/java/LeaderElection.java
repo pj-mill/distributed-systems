49975ac72d53e8bd4883ffde5e5f4e300aa03845
@Apollo013 <paul.j.millar@gmail.com>
2024-02-01 11:09:54 +0000

    Implemented First Part of the Leader Election Dist. Algorithm

--- leader.election/src/main/java/LeaderElection.java
+++ leader.election/src/main/java/LeaderElection.java
@@ -2,6 +2,8 @@
 import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Zookeeper Client Threading Model & Zookeeper Java API
@@ -9,15 +11,41 @@ import java.io.IOException;
 public class LeaderElection implements Watcher {
     private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
     private static final int SESSION_TIMEOUT = 3000;
+    private static final String ELECTION_NAMESPACE = "/election";
     private ZooKeeper zooKeeper;
+    private String currentZnodeName;
 
     public static void main(String[] arg) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection = new LeaderElection();
 
         leaderElection.connectToZookeeper();
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
         leaderElection.run();
         leaderElection.close();
         System.out.println("Disconnected from Zookeeper, exiting application");
+    }
+
+    public void volunteerForLeadership() throws KeeperException, InterruptedException {
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        zooKeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("Created znode " + znodePrefix);
+        this.currentZnodeName = znodePrefix.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    public void electLeader() throws KeeperException, InterruptedException {
+        List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+        
+        Collections.sort(children);
+        String smallestChild = children.get(0);
+
+        if (smallestChild.equals(currentZnodeName)) {
+            System.out.println("I am the leader");
+            return;
+        }
+        
+        System.out.println("I am not the leader, " + smallestChild + " is the leader");
     }
 
     public void connectToZookeeper() throws IOException {
