fb5a7ce8e1138f00c260b305a2509945a3eec037
@Apollo013 <paul.j.millar@gmail.com>
2024-02-16 07:16:58 +0000

    Added AutoHealer / Leader ReElection Examples

--- leader.election/src/main/java/Program.java
+++ leader.election/src/main/java/Program.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
-
 import org.apache.zookeeper.KeeperException;
 
 public class Program {
+    
     public static void main(String[] arg) throws IOException, KeeperException, InterruptedException  {
+        
         // new LeaderElection();
 
-        new Watchers();
+        // new Watchers();
+
+        new LeaderReelection();
         
     }
 }
