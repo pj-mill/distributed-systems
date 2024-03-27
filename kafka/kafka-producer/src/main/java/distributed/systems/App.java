b5ccd83b6fdcb8bc18767d8960384abdc984ea1a
@Apollo013 <paul.j.millar@gmail.com>
2024-03-27 10:48:22 +0000

    Added Producer Implementation with Specified Partitioning

--- kafka/kafka-producer/src/main/java/distributed/systems/App.java
+++ kafka/kafka-producer/src/main/java/distributed/systems/App.java
@@ -1,13 +1,125 @@
 package distributed.systems;
 
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    // public static void main( String[] args )
+    // {
+    //     System.out.println( "Hello World!" );
+    // }
+
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    // Define the Kafka topic we're going to send messages to
+    private static final String TOPIC = "events";
+
+    // List of Kafka addresses our producers will use to establish the initial connection to the entire Kafka
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092,127.0.0.1:9093,127.0.0.1:9094";
+
+    public static void main(String[] args) {
+        // logger.trace("We've just greeted the user!");
+        // logger.debug("We've just greeted the user!");
+        // logger.info("We've just greeted the user!");
+        // logger.warn("We've just greeted the user!");
+        // logger.error("We've just greeted the user!");
+        // logger.fatal("We've just greeted the user!");
+
+        Producer<Long, String> kafkaProducer = createKafkaProducer(BOOTSTRAP_SERVERS);
+
+        try {
+            produceMessages(10, kafkaProducer);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            kafkaProducer.flush();
+            kafkaProducer.close();
+        }
     }
+
+    public static void produceMessages(int numberOfMessages, Producer<Long, String> kafkaProducer) throws ExecutionException, InterruptedException {
+        // define the partition we're going to send messages to
+        int partition = 0;
+
+        // create a loop which will have as many iterations as the number of messages
+        for (int i = 0; i < numberOfMessages; i++) {
+            logger.debug("Creating Message! "+ numberOfMessages) ;
+            long key = i;
+            String value = String.format("event %d", i);
+
+            long timeStamp = System.currentTimeMillis();
+
+            // create a producer record
+            ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, partition, timeStamp, key, value);
+
+            //String msg = String.format("Record with (key: %s, value: %s), was sent to (partition: %d, offset: %d",
+            //record.key(), record.value(), recordMetadata.partition(), recordMetadata.offset());
+
+            //System.out.println(msg);
+
+            //logger.info(msg);
+
+            // send the record to the Kafka topic
+            // RecordMetadata tells us where that record has landed in our distributed Kafka topic
+            try {
+                 kafkaProducer.send(record, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        if (e != null) {
+                            System.out.println("Error producing to topic " + recordMetadata.topic());
+                            e.printStackTrace();
+                        }
+                    }
+                }).get();
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage());
+                //e.printStackTrace();
+            }
+            //RecordMetadata recordMetadata = kafkaProducer.send(record).get();
+
+            // System.out.println(String.format("Record with (key: %s, value: %s), was sent to (partition: %d, offset: %d",
+            //         record.key(), record.value(), recordMetadata.partition(), recordMetadata.offset()));
+
+            //String msg = String.format("Record with (key: %s, value: %s), was sent to (partition: %d, offset: %d",
+            //record.key(), record.value(), recordMetadata.partition(), recordMetadata.offset());
+
+            //System.out.println(msg);
+
+            //logger.info(msg);
+
+        }
+    }
+
+    public static Producer<Long, String> createKafkaProducer(String bootstrapServers) {
+        Properties properties = new Properties();
+
+        // List of Kafka addresses our producers will use to establish the initial connection to the entire Kafka
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        // client ID config which is just a human readable name for our producer which is used mainly for logging purposes Kafka        
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "events-producer");
+
+        // we need to tell the Kafka library how to serialize that object to a binary format that can be    
+        // sent over the network so since we are going to create a producer of keys of        
+        // type long and values of type string we're going to set the third property        
+        // which is the key serializer class to long serializer        
+        // and similarly we will set the value serializer class to the name of the        
+        // string serializer class and that's it our configuration properties are ready
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return new KafkaProducer<>(properties);
+    }    
 }
