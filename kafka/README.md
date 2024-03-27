7f5785832b495d5709bcd68b0cbb4013e3c644fe
@Apollo013 <paul.j.millar@gmail.com>
2024-03-27 12:12:12 +0000

    Adding consumer implementation

--- kafka/README.md
+++ kafka/README.md
@@ -3,3 +3,12 @@
 We have 2 maven projects. The first is a Kafka Producer Broker and the second a Consumer service.
 
 You will need to have Kafka installed locally first. Use the property files in the config folder to help you get setup.
+
+### Scope
+
+1. Message Producing and sending
+2. Topic partitioning
+3. Consuming messages
+4. Load balancing
+5. Publish / Subscribe pattern
+6. Consumer commit failure
--- kafka/kafka-consumer/src/main/java/distributed/systems/App.java
+++ kafka/kafka-consumer/src/main/java/distributed/systems/App.java
@@ -1,11 +1,8 @@
 package distributed.systems;
 
-import java.util.Properties;
-import java.util.concurrent.ExecutionException;
-
 import org.apache.kafka.clients.consumer.*;
-import org.apache.kafka.common.serialization.LongSerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -17,8 +14,54 @@ import org.apache.logging.log4j.Logger;
 public class App 
 {
     private static final Logger logger = LogManager.getLogger(App.class);
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    private static final String TOPIC = "events";
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092,127.0.0.1:9093,127.0.0.1:9094";
+
+    public static void main(String[] args) {
+        String consumerGroup = "defaultConsumerGroup";
+        if (args.length == 1) {
+            consumerGroup = args[0];
+        }
+
+        logger.info("Consumer is part of consumer group " + consumerGroup);
+
+        Consumer<Long, String> kafkaConsumer = createKafkaConsumer(BOOTSTRAP_SERVERS, consumerGroup);
+
+        consumeMessages(TOPIC, kafkaConsumer);
+    }
+
+    public static void consumeMessages(String topic, Consumer<Long, String> kafkaConsumer) {        
+        kafkaConsumer.subscribe(Collections.singletonList(topic));
+
+        // Continuiously poll for new records
+        while (true) {
+            ConsumerRecords<Long, String> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(1));
+
+            if (consumerRecords.isEmpty()) {
+                logger.info("No new records found");
+            }
+
+            for (ConsumerRecord<Long, String> record : consumerRecords) {
+                logger.info(String.format("Received record (key: %d, value: %s, partition: %d, offset: %d",
+                        record.key(), record.value(), record.partition(), record.offset()));
+                        System.out.println((String.format("Received record (key: %d, value: %s, partition: %d, offset: %d",
+                        record.key(), record.value(), record.partition(), record.offset())));
+            }
+
+            kafkaConsumer.commitAsync();
+        }
+    }
+
+    public static Consumer<Long, String> createKafkaConsumer(String bootstrapServers, String consumerGroup) {
+        Properties properties = new Properties();
+
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroup);
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+        return new KafkaConsumer<>(properties);
     }
 }
